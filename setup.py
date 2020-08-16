3509e60aa29a882bd5411d25f450c18bcf56292d
zinklu <zinkworld@live.cn>
2020-08-16 17:21:57 +0800

    修改了部分上传只pypi的打包错误

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ setup(
     name='flask_serializer',  # How you named your package folder (MyLib)
     packages=["flask_serializer", "flask_serializer.utils", "flask_serializer.mixins", "flask_serializer.func_field",
               "flask_serializer.cache_object"],
-    version='0.0.4',  # Start with a small number and increase it with every change you make
+    version='0.0.5.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A Flask serializer built with marshmallow and flask-sqlalchemy",
+    long_description_content_type='text/markdown',
     # Give a short description about your library
     author='ZinkLu',  # Type in your name
     author_email='zinkworld@live.cn',  # Type in your E-Mail
     url='https://github.com/ZinkLu',  # Provide either the link to your github or to your website
-    download_url='https://github.com/ZinkLu/Flask-Serializer/archive/0.0.1.tar.gz',  # I explain this later on
+    download_url='https://github.com/ZinkLu/Flask-Serializer/archive/v0.0.5.tar.gz',  # I explain this later on
     keywords=['Flask', 'extension', 'serializer', "marshmallow", "flask-sqlalchemy"],
     # Keywords that define your package best
     install_requires=[  # I get to this in a second
